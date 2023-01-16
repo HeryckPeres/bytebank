15cb5ceb7cfab069915e2d1d05b5dd5e20df1396
HeryckPeres <heryck_peres@hotmail.com>
2023-01-16 00:13:17 -0300

    Inicio

---
+++ ByteBank/ContaCorrente.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    class ContaCorrente
+    {
+        public int numero_agencia;
+        public string conta;
+        public string Titular;
+        public double saldo;
+
+        public void Depositar(double valor)
+        {
+            this.saldo += valor;
+        }
+
+        public bool Saque(double valor)
+        {
+            if (valor <= this.saldo)
+            {
+                this.saldo -= valor;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+    }
+}
---
+++ ByteBank/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDoAndre = new ContaCorrente();
+
+            contaDoAndre.Titular = "Andre Silva";
+            contaDoAndre.numero_agencia = 15;
+            contaDoAndre.conta = "1010-x";
+            contaDoAndre.saldo = 100;
+
+
+
+            Console.WriteLine("Saldo da conta corrente: " + contaDoAndre.saldo );
+
+            contaDoAndre.Depositar(100);
+
+            Console.WriteLine("Saldo da conta corrente: " + contaDoAndre.saldo);
+
+            if (contaDoAndre.Saque(50) == true)
+            {
+                Console.WriteLine("Saldo apos o saque: " + contaDoAndre.saldo);
+            }
+            else
+            {
+                Console.WriteLine("Saque Invalido.");
+            }
+     
+
+
+
+        }
+    }
+}
